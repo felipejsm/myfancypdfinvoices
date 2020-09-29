d7371372e65631b0c7a249828f4bd34684f76b49
felipejsm <felipejsm41@gmail.com>
2020-09-29 07:50:49 -0300

    Rendering HTML Pages: servlet and tomcat

---
+++ src/main/java/com/felipe/MyFirstServlet.java
@@ -0,0 +1,21 @@
+package com.felipe;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.Exception;
+
+public class MyFirstServlet extends HttpServler {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		response.getWriter().print(
+				"<html>\n" +
+				"<body>\n" +
+				"<h1>Hello World</h1>\n" +
+				"<p>This is my very first, embedded Tomcat, HTML page!</p>\n" +
+				"</body>\n" +
+				"</html>");
+	}
+}
