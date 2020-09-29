97490ce144c1fb59db3010ce59232620bb7bd083
felipejsm <felipejsm41@gmail.com>
2020-09-29 07:53:58 -0300

    Fixing some typos

--- src/main/java/com/felipe/MyFirstServlet.java
+++ src/main/java/com/felipe/MyFirstServlet.java
@@ -3,9 +3,9 @@ package com.felipe;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.Exception;
+import java.io.IOException;
 
-public class MyFirstServlet extends HttpServler {
+public class MyFirstServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
